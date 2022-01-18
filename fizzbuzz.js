17cd913399c737e502b452d6f0827b2fee5d329f
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-18 10:36:39 +0000

    added program that prints Hello World to the console

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,1 @@
-console.log('Hello World');
-
-for(let i = 1; i <= 100; i++) {
-    if(i % 3 == 0) {
-        console.log('Fizz');
-    } else {
-        console.log(i);
-    }
-}
-
+console.log('Hello World!');
