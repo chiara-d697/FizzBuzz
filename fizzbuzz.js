1f59fccaf96ea4dd7dca70d16f9ada2772dba784
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-18 21:45:45 +0000

    installed prompt-sync, program now allows for  user inputs, user can specify a max number and specify which rules the program will implement

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,35 +1,47 @@
-console.log('Hello World!');
+// const readline = require('readline-sync');
+const ps = require('prompt-sync');
+
+const prompt = ps();
+
+let userChoice = prompt("Please specify a maximum number:");
+let userNumChoice = +userChoice;
+
+let userRuleChoice = prompt("Please specify the numbers, separated by spaces, that you'd like us to implement rules for:");
+const userRuleChoiceArr = userRuleChoice.split(" ");
+console.log(userRuleChoiceArr);
 
 function isDivisibleBy(a, b) {
     return a % b === 0;
 }
 
-for( let i = 1; i <= 300; i++) {
+function checkRuleChoice(numStr) {
+    return userRuleChoiceArr.indexOf(numStr) >= 0;
+}
+
+for( let i = 1; i <= userNumChoice; i++) {
     let result = [];
     
-    if(isDivisibleBy(i,3)) {
+    if(isDivisibleBy(i,3) && checkRuleChoice("3")) {
         result.push("Fizz");
     }
-    if(isDivisibleBy(i,5)) {
+    if(isDivisibleBy(i,5) && checkRuleChoice("5")) {
         result.push("Buzz");
     }
-    if(isDivisibleBy(i,7)) {
+    if(isDivisibleBy(i,7) && checkRuleChoice("7")) {
         result.push("Bang");
     }
 
-    if(isDivisibleBy(i,11)) {
+    if(isDivisibleBy(i,11) && checkRuleChoice("11")) {
         result = ["Bong"];
     }
 
-    if(isDivisibleBy(i,13)) {
+    if(isDivisibleBy(i,13) && checkRuleChoice("13")) {
         const indexB = result.findIndex(item => item[0] === "B");
         result.splice(indexB, 0, "Fezz");
-
     }
 
-    if(isDivisibleBy(i, 17)) {
+    if(isDivisibleBy(i,17) && checkRuleChoice("17")) {
         result.reverse();
-
     }
 
     if(result.length === 0) {
@@ -40,10 +52,8 @@ for( let i = 1; i <= 300; i++) {
 }
 
 
-// let exampleArr = ["Fizz", "Buzz", "Bong"];
-// const indexB = exampleArr.findIndex(item => item[0] === "B");
-// exampleArr.splice(indexB, 0, "Fezz");
-// console.log(exampleArr);
+
+
 
 
 
