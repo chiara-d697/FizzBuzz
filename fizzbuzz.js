7a9fed29b600812cb0a4fa18561f194efb301bd1
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-18 12:03:18 +0000

    refactored solution to increase readability following presentation and added functionality for multiples of 11 to display Bong

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,32 @@
 console.log('Hello World!');
+
+function isDivisibleBy(a, b) {
+    return a % b === 0;
+}
+
+for( let i = 1; i <= 100; i++) {
+    let result = "";
+    
+    if(isDivisibleBy(i,3)) {
+        result += "Buzz";
+    }
+    if(isDivisibleBy(i,5)) {
+        result += "Fizz";
+    }
+    if(isDivisibleBy(i,7)) {
+        result += "Bang";
+    }
+
+    if(isDivisibleBy(i,11)) {
+        result = "Bong";
+    }
+
+    if(result === "") {
+        result = i;
+    }
+
+
+    
+    console.log(result);
+}
+
